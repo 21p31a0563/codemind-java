9cb4e8ece7ae5787f89a68ab9802b02956f76a1f
SAYED AHAMAD ALISHA <98599851+21p31a0563@users.noreply.github.com>
2023-01-31 11:43:53 +0530

    import java.util.*; class area {     public static void main(String args[])     {         int r;         Scanner sc=new Scanner (System.in);         r=sc.nextInt();         double a=(float)(3.14*r*r);         System.out.format("%.2f",a);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class area
+{
+    public static void main(String args[])
+    {
+        int r;
+        Scanner sc=new Scanner (System.in);
+        r=sc.nextInt();
+        double a=(float)(3.14*r*r);
+        System.out.format("%.2f",a);
+    }
+}
