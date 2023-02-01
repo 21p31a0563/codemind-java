e12f3f7bc1b645a76571beb5f47d8aa9d61f6847
SAYED AHAMAD ALISHA <98599851+21p31a0563@users.noreply.github.com>
2023-02-01 12:01:48 +0530

    import java.util.*; class handshakes {     public static void main(String args[])     {         int a;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         int b=(a*(a-1))/2;         System.out.println(b);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class handshakes
+{
+    public static void main(String args[])
+    {
+        int a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        int b=(a*(a-1))/2;
+        System.out.println(b);
+    }
+}
