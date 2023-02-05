86c7bd6ff608dd6f41832ab9592067abab32e909
SAYED AHAMAD ALISHA <98599851+21p31a0563@users.noreply.github.com>
2023-02-05 13:21:43 +0530

    import java.util.*; class tables {     public static void main(String args[])     {         int n,r,i;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         r=sc.nextInt();
            for(i=1;i<=r;i+=2)
            {
             System.out.println(n+" "+"x"+" "+i+" "+"="+" "+n*i);   
            }}
        }

---
+++ Tables.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class tables
+{
+    public static void main(String args[])
+    {
+        int n,r,i;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        r=sc.nextInt();
+        
+        
+        for(i=1;i<=r;i+=2)
+        {
+         System.out.println(n+" "+"x"+" "+i+" "+"="+" "+n*i);   
+        }}
+    }
